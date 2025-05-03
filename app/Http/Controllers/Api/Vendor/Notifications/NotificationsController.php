8e0d5a083181f92f04bfab3e7c64a6b2ae7fa9c9
unknown <saumiljain375@gmail.com>
2025-05-04 00:25:53 +0530

    vendor forgotpassword and notification API

--- app/Http/Controllers/Api/Vendor/Notifications/NotificationsController.php
+++ app/Http/Controllers/Api/Vendor/Notifications/NotificationsController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Api\Vendor\Notifications;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\Vendor\NotificationRequest;
+use App\Http\Resources\Api\Vendor\Notifications\NotificationsResource;
 use Illuminate\Http\Request;
 use App\Services\Api\Vendor\Authservice;
 use Exception;
@@ -15,5 +17,33 @@ class NotificationsController extends Controller
     public function __construct(AuthService $authService)
     {
         $this->authService = $authService;
+    }
+
+    public function getNotificationList(Request $request) {
+
+        try{
+            $notificationList = $this->authService->getNotificationList($request);
+            return success(pagination(NotificationsResource::class, $notificationList), trans('messages.list',['attribute'=>'Notifications']), config('code.SUCCESS_CODE'));
+        } catch (Exception $e) {
+            return fail([], $e->getMessage(), config('code.EXCEPTION_ERROR_CODE'));
+        }
+    }
+
+    public function markReadNotification(NotificationRequest  $request) {
+        try{
+            $markReadNotification = $this->authService->markReadNotification($request);
+            return success([], trans('messages.mark_read'), config('code.SUCCESS_CODE'));
+        } catch (Exception $e) {
+            return fail([], $e->getMessage(), config('code.EXCEPTION_ERROR_CODE'));
+        }
+    }
+
+    public function deleteNotification(NotificationRequest  $request) {
+        try{
+            $deleteNotification = $this->authService->deleteNotification($request);
+            return success([], trans('messages.deleted',['attribute' => 'Notification']), config('code.SUCCESS_CODE'));
+        } catch (Exception $e) {
+            return fail([], $e->getMessage(), config('code.EXCEPTION_ERROR_CODE'));
+        }
     }
 }
---
+++ app/Http/Resources/Api/Vendor/Notifications/NotificationsResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources\Api\Vendor\Notifications;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class NotificationsResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        // return parent::toArray($request);
+        return [
+            'id' => $this->notification->id,
+            'title' => $this->notification->title,
+            'message' => $this->notification->message,
+            'type' => $this->notification->type,
+            'status' => $this->status, // 0 = unseen, 1 = seen
+            'sent_at' => $this->created_at->toDateTimeString(),
+        ];
+    }
+}
